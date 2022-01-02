ce4289d09953b7b3bc238f58de4f003d608fb41e
SiyuLiu0 <527100746@qq.com>
2022-01-01 18:15:18 -0800

    Added haome page template

---
+++ src/main/java/io/lemonT/coronavirustracker/controllers/HomeController.java
@@ -0,0 +1,21 @@
+package io.lemonT.coronavirustracker.controllers;
+
+import io.lemonT.coronavirustracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("testName", "Test");
+//        model.addAttribute("locationStat", coronaVirusDataService.getAllStats());
+        return  "home";
+    }
+}
